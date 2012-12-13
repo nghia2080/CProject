2613499a53d363e42a69f602fc0f79b7ea488e63
emismith <hunglh01011@fpt.edu.vn>
2012-12-13 06:36:49 -0800

    update semantic

--- antares/Antares/WIP/Source/Trunk/Antares/Antares/Converters/TaskToColorConverter.cs
+++ antares/Antares/WIP/Source/Trunk/Antares/Antares/Converters/TaskToColorConverter.cs
@@ -8,16 +8,33 @@ namespace Antares.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
-            var model = value as TaskModel;
-            
-            switch (model.Category.ToString())
+            int cateID = 0;
+            if (value is TaskModel)
             {
-                case "10": return "../Assets/TaskTemplate/requirement_icon.png";
-                case "11": return "../Assets/TaskTemplate/design_icon.png";
-                case "12": return "../Assets/TaskTemplate/implementation_icon.png";
-                case "13": return "../Assets/TaskTemplate/verification_icon.png";
-                case "14": return "../Assets/TaskTemplate/maintenance_icon.png";
-                default: return "../Assets/TaskTemplate/task_icon.png";
+                var model = value as TaskModel;
+                cateID = model.Category;
+
+            }
+            else if (value is GroupCollection)
+            {
+                var model = value as GroupCollection;
+                cateID = model.Id;
+            }
+
+            switch (cateID)
+            {
+                case 10:
+                    return "../Assets/TaskTemplate/requirement_icon.png";
+                case 11:
+                    return "../Assets/TaskTemplate/design_icon.png";
+                case 12:
+                    return "../Assets/TaskTemplate/implementation_icon.png";
+                case 13:
+                    return "../Assets/TaskTemplate/verification_icon.png";
+                case 14:
+                    return "../Assets/TaskTemplate/maintenance_icon.png";
+                default:
+                    return "../Assets/TaskTemplate/task_icon.png";
             }
         }
 
